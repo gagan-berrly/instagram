1c9cf56d76c37a9032191371b65cc155d77dc486
gagandeep <gagandeep.dass.7e3@itb.cat>
2022-03-13 21:50:56 +0100

    usuario puede visualizar la imagen de la publicacion antes de subirla

--- resources/views/image/create.blade.php
+++ resources/views/image/create.blade.php
@@ -10,10 +10,15 @@
                 <div class="card-body">
                     <form method="post" action="{{ route('post.save') }}" enctype="multipart/form-data">
                         @csrf
+                        <div id ="preview-container">
+                            <img id="select_image" style="display:block; padding:10px; margin-left:auto; margin-right:auto; margin-top:30px; margin-bottom:30px; width:300px;"/>
+                            </div>
                         <div class="form-group row">
                             <label for="image_path" class="col-md-3 col-form-label text-md-right">{{__('upload.image')}}</label>
                             <div class="col-md-7">
-                                <input id="image_path" type="file" name="image_path" class="form-control{{ $errors->has('image_path') ? ' is-invalid' : '' }}" required/>
+                                
+                                <label for="image_path" class="btn btn-outline-primary btn-lg btn-block" ><i style="font-size:25px; font-style: normal;" id="upload-btn"class="bi bi-cloud-arrow-up-fill"></i></label>
+                                <input id="image_path" type="file" name="image_path" class="form-control{{ $errors->has('image_path') ? ' is-invalid' : '' }}" style="visibility:hidden; position:absolute;" onchange="loadFile(event)" required/>
                                 
                                 @if($errors->has('image_path'))
                                     <span class="invalid-feedback" role="alert">
@@ -50,5 +55,18 @@
     </div>
     <hr>
 </div>
+<script type="text/javascript">
+    var loadFile = function(event) {
+        var select_image = document.getElementById('select_image');
+        var preview_container = document.getElementById('preview-container');
+        select_image.src = URL.createObjectURL(event.target.files[0]);
+        select_image.style.height = '200px';
+        select_image.onload = function() {
+            select_image.style.removeProperty('height');
+            URL.revokeObjectURL(select_image.src) // free memory
+        }
+
+    };
+</script>
 @endsection
  
